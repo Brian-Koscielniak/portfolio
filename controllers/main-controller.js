07ae70e2d4cdebcb40a4c9a33cb06926450f3d58
Brian Koscielniak <bkoscielniak31@gmail.com>
2015-12-29 17:15:26 -0500

    Built basic structure and markup

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -1,6 +1,18 @@
-exports.notFound = function(req, res){
-	res.render("layout.jade");
-}
 exports.home = function(req, res){
 	res.render("index.jade");
+}
+exports.info = function(req, res){
+	res.render("info.jade");
+}
+exports.projects = function(req, res){
+	res.render("projects.jade");
+}
+exports.contact = function(req, res){
+	res.render("contact.jade");
+}
+exports.blog = function(req, res){
+	res.redirect(' https://koscielniak.wordpress.com/');
+}
+exports.notFound = function(req, res){
+	res.render("layout.jade");
 }
--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,9 @@ module.exports = function(app){
 	/* GETs */
 	app.get('/', function(req, res){controller.home(req, res)});
 	app.get('/home', function(req, res){controller.home(req, res)});
+	app.get('/info', function(req, res){controller.info(req, res)});
+	app.get('/projects*', function(req, res){controller.projects(req, res)});
+	app.get('/contact', function(req, res){controller.contact(req, res)});
+	app.get('/blog', function(req, res){controller.blog(req, res)});
 	app.get('*', function(req, res){controller.notFound(req, res)});
 }
