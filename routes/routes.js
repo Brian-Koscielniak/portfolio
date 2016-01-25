2a96db61192f6066cc407fc25bd9d4c140dc1033
Brian Koscielniak <bkoscielniak31@gmail.com>
2016-01-25 16:55:31 -0500

    Jankily spliced in live demo of realtor application

--- routes/routes.js
+++ routes/routes.js
@@ -8,8 +8,21 @@ module.exports = function(app){
 	app.get('/showcase*', function(req, res){controller.showcase(req, res)});
 	app.get('/contact', function(req, res){controller.contact(req, res)});
 	app.get('/blog', function(req, res){controller.blog(req, res)});
+		/* Temp GETs */// Temp GETs will be removed once a superior solution for live demos is found.
+			app.get('/realtor', function(req, res){ res.render('../projects/realtorApp/views/realtorApp.jade') });
+		/* Temp GETs */
 	app.get('*', function(req, res){controller.notFound(req, res)});
 
 	/* POSTs */
 	app.post('/contact', function(req, res){controller.contactPost(req, res)});
+		/* Temp POSTs */// Temp POSTs will be removed once a superior solution for live demos is found.
+			app.post("/rest", function(req, res){
+				var request = require('request');
+				request(req.body.data, function (error, response, body) {
+					if (!error && response.statusCode === 200) {
+						res.send(body);
+					}
+				});
+			});
+		/* Temp POSTs */
 }
