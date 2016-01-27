3afd842befef4aaa40980fb214957a9107cecf3e
Brian Koscielniak <bkoscielniak31@gmail.com>
2016-01-27 15:48:08 -0500

    Many minor improvements throughout site

--- routes/routes.js
+++ routes/routes.js
@@ -5,11 +5,12 @@ module.exports = function(app){
 	app.get('/', function(req, res){controller.home(req, res)});
 	app.get('/home', function(req, res){controller.home(req, res)});
 	app.get('/about', function(req, res){controller.about(req, res)});
-	app.get('/showcase*', function(req, res){controller.showcase(req, res)});
+	app.get('/showcase', function(req, res){controller.showcase(req, res)});
 	app.get('/contact', function(req, res){controller.contact(req, res)});
 	app.get('/blog', function(req, res){controller.blog(req, res)});
 		/* Temp GETs */// Temp GETs will be removed once a superior solution for live demos is found.
-			app.get('/realtor', function(req, res){ res.render('../projects/realtorApp/views/realtorApp.jade') });
+			app.get('/showcase/realtor', function(req, res){ res.render('../projects/realtorApp/views/realtorApp.jade') });
+			app.get('/showcase/*', function(req, res){ res.send('content unavailable') });
 		/* Temp GETs */
 	app.get('*', function(req, res){controller.notFound(req, res)});
 
